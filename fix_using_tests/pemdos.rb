e262316c3b5cf4964c8eefc609b83478ba141d0e
Alice Perlov <aperlov23@gmail.com>
2020-07-26 01:42:24 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,10 +1,13 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def snake_it_up(string)
+  binding.pry
   if string[0] == "s"
-    10 * "s" + "string"
+
+    10 * "s" + string
   else
-    "string"
+    string
   end
 end
 snake_it_up(10)
